99b488988f85549f3a71d8861a3adfc50128edf6
Sergey Galkin <sergey.v.galkin@yandex.ru>
2014-07-23 21:36:35 +0000

    queue-client: initial commit

---
+++ queue-client/httpclient.fs
@@ -0,0 +1,29 @@
+﻿module httpclient
+
+open System
+open System.Net.Http
+
+let send verb (c:HttpClient) url =
+    async{
+        let u = new Uri(url)
+        use req = new HttpRequestMessage(verb, u)
+        let send = c.SendAsync(req)
+        let! resp = Async.AwaitTask send
+        return resp
+    }
+
+let get = send HttpMethod.Get
+let post = send HttpMethod.Post
+let delete = send HttpMethod.Delete
+let put = send HttpMethod.Put
+
+type Client() =
+    let client = new HttpClient()
+
+    member self.get = get client
+    member self.post = post client
+    member self.put = put client
+    member self.delete = delete client
+
+    interface System.IDisposable with
+        member self.Dispose() = client.Dispose()
---
+++ queue-client/main.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.    
+
+let tuple a = (a)         
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    //use client = new System.Net.Http.HttpClient()
+    //using ("https://google.com" |> tuple |> client.GetAsync |> Async.AwaitTask |> Async.RunSynchronously) (fun x -> 
+
+    use client = new httpclient.Client()
+    using ("https://google.com" |> client.get |> Async.RunSynchronously) (fun x ->
+        let content = x.Content.ReadAsStringAsync()
+        printf "%s" content.Result
+    )
+    0 // return an integer exit code
